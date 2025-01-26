7baa84854e91fe1e28766441004879be2e653f27
Peter <peterhoang2704@gmail.com>
2025-01-25 20:41:31 -0600

    Fixed CommonSubsequence error where in Test 2, when tex1 = "almanacs" and text2 = "albatross", the output is miscount.

--- src/CommonSubsequence.java
+++ src/CommonSubsequence.java
@@ -2,19 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class CommonSubsequence {
     public static void main(String[] args) {
-        String text1 = "almanac";
+        String text1 = "almanacs";
         String text2 = "albatross";
         System.out.println(checkCommonSub(text1,text2));
     }
 
     public static int checkCommonSub(String text1, String text2) {
+        int [] textArray = new int[text2.length()];
         int count = 0;
         for (int i = 0; i < text1.length(); i++){
-            for (int j = 0; j < text2.length(); j++) {
-                if (text1.charAt(i) == text2.charAt(j))
+            int count2 = 0;
+            for (int j = 0; j < textArray.length; j++) {
+                if(count2 < textArray[j])
                 {
-                    count++;
-                    break;
+                    count2 = textArray[j];
+                } else if (text1.charAt(i) == text2.charAt(j)){
+                    count = count2 + 1;
+                    textArray[j] = count;
                 }
             }
         }
