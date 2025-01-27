eb5120772b73d331c3f3dce9258572e22218f824
Peter <peterhoang2704@gmail.com>
2025-01-26 19:43:52 -0600

    Added comments to explain the code which helps going through each steps.

--- src/CommonSubsequence.java
+++ src/CommonSubsequence.java
@@ -8,16 +8,24 @@ public class CommonSubsequence {
     }
 
     public static int checkCommonSub(String text1, String text2) {
+        // Create an array to store  substring lengths for text2
         int [] textArray = new int[text2.length()];
+        // Variable to store the maximum length of the common substring
         int count = 0;
+        //Loop through every character of text1
         for (int i = 0; i < text1.length(); i++){
+            // Variable to track the longest common substring ending
             int count2 = 0;
             for (int j = 0; j < textArray.length; j++) {
+                // Update count2 to store the maximum value seen in textArray at that point
                 if(count2 < textArray[j])
                 {
                     count2 = textArray[j];
+                    //If matched
                 } else if (text1.charAt(i) == text2.charAt(j)){
+                    // Update the count of the common substring length
                     count = count2 + 1;
+                    // Update the array with the new count
                     textArray[j] = count;
                 }
             }
--- src/notFibbonacci.java
+++ src/notFibbonacci.java
@@ -2,25 +2,31 @@ import java.util.Scanner;
 
 public class notFibbonacci {
     public static void main(String[] args) {
+        //Using scanner to ask user for the number of term
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the number of terms");
         int terms = scanner.nextInt();
 
+        //Creating the notFib sequence and print it out
         long[] sequence = generateNotFibonacci(terms);
         for (long num : sequence) {
             System.out.print(num + " ");
         }
         System.out.println();
 
-        System.out.print("Enter a number of its position: ");
+        //Using a scanner to ask the user the number
+        System.out.print("Enter a number: ");
         long number = scanner.nextLong();
 
+        //Using checkOutput function to return the position in the array of that number
         int position = checkOutput(number);
         System.out.println("Position: " + position);
     }
 
+    //The method is using loop (iterative) to add terms into an array.
     public static long[] generateNotFibonacci(int terms) {
+        //The array that holds all the numbers
         long[] sequence = new long[terms];
 
         // Base cases
@@ -29,14 +35,16 @@ public class notFibbonacci {
             sequence[1] = 1;
         }
 
+        //Looping and using the formula to calculate for the next term based on the previous two terms.
         for (int i = 2; i < terms; i++) {
             sequence[i] = (3 * sequence[i - 1]) + (2 * sequence[i - 2]);
         }
 
         return sequence;
     }
-
+    //Recursive way to do notFib, I find that it's easier to do #4 recursively.
     public static long notFib(int n) {
+        //Base case
         if (n == 0) {
             return 0;
         } else if (n == 1) {
@@ -47,22 +55,21 @@ public class notFibbonacci {
     }
 
     public static int checkOutput(long pos) {
+        //the variable i representing is used to iterate through the positions in the NotFibonacci sequence
         int i = 0;
-
         while (true) {
-            long a = notFib(i);
-
-            if (a >= pos) {
-                if (a == pos) {
+            //Using the notFib(i) to compute the value at index, and check if that value is > or = to pos
+            //if lower than the pos value, then we keep increasing i to check for the next num
+            //when it greater or equal than, it returns the i+1 position
+            // because positions are 0-based in the sequence.
+            if (notFib(i) >= pos) {
                     return i + 1;
-                } else {
-                    return i;
-                }
             }
-
-            i++;
-        }
+            else
+                i++;
+         }
     }
 }
+
 
 
